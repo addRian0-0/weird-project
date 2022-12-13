da25846ab79ca8dfc17469b09c8b94b6cabf8ee9
Gotacme31 <abatizj2000@alumno.ipn.mx>
2022-12-13 08:42:27 -0600

    do commit

--- index.js
+++ index.js
@@ -3,23 +3,23 @@ const path = require('path');
 const morgan = require('morgan');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+app.set('port', process.env.PORT || 4000);
 
 //app.set('port', );
-app.set('views', path.join(__dirname, 'public/views'));
+app.set('views', path.join(__dirname, './src/public/views'));
 app.set('view engine', '.ejs');
 
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
-app.use('/', require('./routes/control.routes'));
+app.use('/', require('./src/routes/control.routes'));
 
 app.use((req,res,next)=>{
     next();
 })
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(PORT,() =>{
-    console.log(`Servidor en el puerto ${PORT}`);
+app.listen(app.get('port'), () => {
+    console.log(`Servidor en el puerto ${app.get('port')}`);
 })

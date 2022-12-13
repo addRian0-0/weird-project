d28ba521597761f893816556e42161f3049c2024
addRian0-0 <addrianun@gmail.com>
2022-12-13 12:39:03 -0600

    zzz

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const morgan = require('morgan');
 app.set('port', process.env.PORT || 4000);
 
 //app.set('port', );
--- src/routes/control.routes.js
+++ src/routes/control.routes.js
@@ -1,6 +1,46 @@
 
 const router = require('express').Router();
-const controlController = require('../controllers/control');
 
-router.get('/', controlController.list);
+router.get('/', async (req, res) => {
+    var d = require('diskinfo');
+
+    d.getDrives((err, aDrives) => {
+        if (err) {
+            console.error(err);
+        }
+        for (var i = 0; i < aDrives.length; i++) {
+            var os = require("os");
+            infoPoke = os.cpus();
+            system = os.platform();
+            arquitectura = os.arch();
+            version = os.release();
+            time = os.uptime();
+            var total = ((os.totalmem()) / 1048576);
+            var mbFree = ((os.freemem()) / 1048576);
+            var mounted = aDrives[i].mounted;
+            var totall = (aDrives[i].blocks / 1024 / 1024 / 1024).toFixed(0) + " gb";
+            var used = (aDrives[i].used / 1024 / 1024 / 1024).toFixed(0) + " gb";
+            var available = (aDrives[i].available / 1024 / 1024 / 1024).toFixed(0) + " gb";
+            var capacity = aDrives[i].capacity;
+            let dataExtra = {
+                dataPoke: infoPoke,
+                dataTotal: total,
+                dataLibre: mbFree,
+                dataDiscTotall: totall,
+                dataDiscMounted: mounted,
+                dataDiscUsed: used,
+                dataDiscAvailable: available,
+                dataDiscCapacity: capacity,
+                dataSystem: system,
+                dataArqui: arquitectura,
+                dataVersion: version,
+                dataTime: time
+            }
+            res.render('index', {
+                dataExtra: dataExtra
+            })
+            break;
+        }
+    });
+});
 module.exports = router;

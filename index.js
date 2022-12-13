00b9f841a56244855aaf1c1c691d7de52da61f3d
Gotacme31 <abatizj2000@alumno.ipn.mx>
2022-12-13 09:18:42 -0600

    tre commit

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const path = require('path');
-const morgan = require('morgan');
-
 const app = express();
 app.set('port', process.env.PORT || 4000);
 
@@ -9,15 +7,11 @@ app.set('port', process.env.PORT || 4000);
 app.set('views', path.join(__dirname, './src/public/views'));
 app.set('view engine', '.ejs');
 
-app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
 app.use('/', require('./src/routes/control.routes'));
 
-app.use((req,res,next)=>{
-    next();
-})
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(app.get('port'), () => {
